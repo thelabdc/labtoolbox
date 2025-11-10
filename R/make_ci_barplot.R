adc60261ba3600ae5633d6b4d89ad82b6a0992bf
alysshuberts <alyssa.huberts@dc.gov>
2025-11-10 14:12:51 -0500

    Coef plot

--- R/make_ci_barplot.R
+++ R/make_ci_barplot.R
@@ -6,31 +6,18 @@
 #' @param treat_arms A character vector for the treatment arms, as reported in the regression
 #' @param ci_level = .95
 #' @return A bar plot with a confidence interval on the treatment arms
+#' @import tidyverse
 #' @export
 # Function to return linear combinations of coefficients, with confidence intervals
-make_ci_barplot <- function(model = sample_model, treat_arms = c("sample_treatment1"),
+make_ci_barplot <- function(model = sample_model_1, treat_arms = c("sample_treatment1"),
                             ci_level = .95,
+                            plot_caption = "Sample Plot Caption",
                             ...){
   # Make sample data in case it's necessary
 
-  set.seed(955)  # for reproducibility
-
-  sample_data <- data.frame(
-    x = rnorm(50, mean = 2, sd = 0.15),  # smaller sample size (50 instead of 100)
-    sample_treatment = factor(sample(c(1, 0), 50, replace = TRUE))
-  ) |>
-    mutate(
-      sample_outcome = ifelse(
-        sample_treatment == 1,
-        x + rnorm(50, mean = 1.5, sd = 2.5) + rnorm(50, mean = 1.5, sd = 5),  # increased noise
-        x + rnorm(50, mean = 1, sd = 2.5) + rnorm(50, mean = 1.5, sd = 5)
-      )
-    )
-
-  sample_model <- lm(sample_outcome ~ sample_treatment, sample_data)
   if(!exists("model")){
     print("Warning: This output uses sample data. Make sure to submit a model for the model parameter.")
-    model <- sample_model
+    model <- sample_model_1
   }
   tidy_model <- broom::tidy(model)
 
@@ -49,30 +36,30 @@ make_ci_barplot <- function(model = sample_model, treat_arms = c("sample_treatme
   }
 
   if(!exists("plot_caption")){
-    plot_caption <- paste("Results from a randomized evaluation using ", n_obs, "observations.")
+    plot_caption <- paste("The Lab @ DC. Results from a randomized evaluation using ", n_obs, "observations.")
   }
 
   # make a table of the relevant linear combinations
 
   coef_table <-
-    tibble(
+    tibble::tibble(
     term = c("Control", treat_arms),
     estimate = as.numeric(unlist(tidy_model[tidy_model$term == "(Intercept)"|tidy_model$term %in% treat_arms,"estimate"])),
     se = as.numeric(unlist(tidy_model[tidy_model$term == "(Intercept)"|tidy_model$term %in% treat_arms,"std.error"]))) |>
-    mutate(
-      linear_combination = case_when(
+    dplyr::mutate(
+      linear_combination = dplyr::case_when(
         term == "Control" ~  estimate,
         term != "Control" ~ estimate + intercept)
       )|>
-    mutate(ci_upper = ifelse(term == "Control", NA, linear_combination + ( qnorm(1- (1-ci_level)/2)*se)),
+    dplyr::mutate(ci_upper = ifelse(term == "Control", NA, linear_combination + ( qnorm(1- (1-ci_level)/2)*se)),
            ci_lower = ifelse(term == "Control", NA, linear_combination - ( qnorm(1- (1-ci_level)/2)*se))
     )
 
   # make the plot
-  coef_plot <- ggplot(coef_table) + geom_bar(aes(x = term, y = linear_combination, fill = term), stat = "identity")+
-    geom_errorbar(data = coef_table, aes(x = term, ymin =ci_lower,ymax = ci_upper), width = .75) +
+  coef_plot <- ggplot2::ggplot(coef_table) + ggplot2::geom_bar(ggplot2::aes(x = term, y = linear_combination, fill = term), stat = "identity")+
+    ggplot2::geom_errorbar(data = coef_table, ggplot2::aes(x = term, ymin =ci_lower,ymax = ci_upper), width = .75) +
     theme_lab(...)+
-    labs(caption = plot_caption,
+    ggplot2::labs(caption = plot_caption,
          title = "Treatment Effect",
          subtitle = "95% Confidence Intervals",
          x = "",
@@ -81,5 +68,3 @@ make_ci_barplot <- function(model = sample_model, treat_arms = c("sample_treatme
   return(coef_plot)
 
 }
-
-
---
+++ R/make_coef_plot.R
@@ -0,0 +1,96 @@
+
+#' make_coef_plot Make a plot of treatment effects with confidence intervals against a dotted line at zero
+#'
+#' This function takes the output from several models and plots their estimated treatment effects with confidence intervals horizontally, across a dotted line at zero.
+#' @param model_list A list of models
+#' @param model_names A character vector with a name for each model
+#' @param treat_vars A character vector of the main treatment variable in each regression. If only one is provided, will be assumed to apply to all regressions.
+#' @param plot_title A title for the plot
+#' @param plot_subtitle A subtitle for the plot
+#' @param plot_caption A caption for the plot
+#' @param return_ci_table If TRUE, returns the table of confidence intervals underlying the plot
+
+#' @return A ggplot object
+#' @import tidyverse
+#' @export
+# Function to return linear combinations of coefficients, with confidence intervals
+make_coef_plot <- function(model_list = list(sample_model_1, sample_model_2, sample_model_3),
+                           model_names = c("Outcome 1", "Outcome 2","Outcome 3"),
+                           treat_vars = c("sample_treatment1", "sample_treatment1", "sample_treatment1"),
+                                          plot_title = "Treatment effects",
+                                          plot_subtitle = "95% confidence intervals",
+                                          plot_caption = "The Lab @ DC. Regression results from a randomized evaluation.",
+                                          return_ci_table = FALSE,
+                                          ...) {
+
+  if(length(treat_vars) == 1){
+    treat_vars <- rep(treat_vars, length(models))
+  }
+
+  # Create an empty data frame where you'll place everything
+  plotting_df <- dplyr::tibble(term = rep(NA, length(model_list)),
+                        estimate = rep(NA, length(model_list)),
+                        upper = rep(NA, length(model_list)),
+                        lower = rep(NA, length(model_list)),
+                        model = c(1:length(model_list)))
+
+  # Iterate through each model in the input list
+  for (i in 1:length(model_list)) {
+    current_model <-model_list[[i]]
+
+    # Perform operations on the current model
+    # For example, extract coefficients
+    plotting_df[i, "term"] <- names(coef(current_model)[treat_vars[i]])
+    plotting_df[i, "estimate"] <- coef(current_model)[treat_vars[i]]
+    plotting_df[i, "upper"] <-coef(current_model)[treat_vars[i]]+
+      (qnorm(1- (1-ci_level)/2)*  coef(summary(current_model))[, "Std. Error"][treat_vars[i]])
+    plotting_df[i, "lower"] <-coef(current_model)[treat_vars[i]]-
+      (qnorm(1- (1-ci_level)/2)*  coef(summary(current_model))[, "Std. Error"][treat_vars[i]])
+    plotting_df <- dplyr::tibble(term = rep(NA, length(model_list)),
+                        estimate = rep(NA, length(model_list)),
+                        upper = rep(NA, length(model_list)),
+                        lower = rep(NA, length(model_list)),
+                        model = c(1:length(model_list)))
+
+  # Iterate through each model in the input list
+  for (i in 1:length(model_list)) {
+    current_model <-model_list[[i]]
+
+    # Perform operations on the current model
+    # For example, extract coefficients
+    plotting_df[i, "term"] <- names(coef(current_model)[treat_vars[i]])
+    plotting_df[i, "estimate"] <- coef(current_model)[treat_vars[i]]
+    plotting_df[i, "upper"] <-coef(current_model)[treat_vars[i]]+
+      (qnorm(1- (1-ci_level)/2)*  coef(summary(current_model))[, "Std. Error"][treat_vars[i]])
+    plotting_df[i, "lower"] <-coef(current_model)[treat_vars[i]]-
+      (qnorm(1- (1-ci_level)/2)*  coef(summary(current_model))[, "Std. Error"][treat_vars[i]])
+  }
+
+    if(exists("model_names")){
+      plotting_df$model <- model_names
+    }else(
+      plotting_df$model <- 1:length(model)
+    )
+    coef_plot <- ggplot2::ggplot(plotting_df) + ggplot2::geom_point(ggplot2::aes(x = model, y = estimate,  color = factor(model)), stat = "identity")+
+      ggplot2::geom_errorbar(data = plotting_df, ggplot2::aes(x = model, ymin =lower,ymax = upper, color = factor(model)), width = .75) +
+      ggplot2::geom_hline(ggplot2::aes(yintercept = 0), linetype = "dotted")+
+      ggplot2::coord_flip() +
+      theme_lab(...)+
+      ggplot2::theme(legend.position = "none")+
+      ggplot2::labs(caption = plot_caption,
+                    title =  plot_title,
+                    subtitle =  plot_subtitle,
+                    x = "",
+                    y = "Treatment Effect",
+                    fill = "")
+
+  }
+
+
+  # Return the collected results
+  if(return_ci_table == TRUE){
+    return(plotting_df)
+  }else
+    return(coef_plot)
+
+}
